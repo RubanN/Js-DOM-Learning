5892f425bb269c3706459f4e3b137cef695df32d
Ruban <80041905+RubanN@users.noreply.github.com>
2021-06-18 21:28:39 +0530

    Create document model

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let newElement = document.createElement('input');
+console.log(newElement);
+document.body.appendChild(newElement);
+let head = document.createElement('h1');
+head = "Hello ruban"
+console.log(head);
+document.write(head)
+let btn = document.createElement('button');
+console.log(btn);
+document.write(btn)
